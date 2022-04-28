6fbd8cc336edc774763d728c6e379013e6fdfcd1
Hajin Kim <rhrnak624@naver.com>
2022-04-28 16:41:42 +0900

    Feat: 메인 페이지 공지 작성 viewmodel #13

--- app/src/main/java/com/mirim/refrigerator/model/Notice.kt
+++ app/src/main/java/com/mirim/refrigerator/model/Notice.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 
 class Notice(var title: String?, var contents: String?) {
 
-    companion object {
-        val notice = Notice("엄마의 잔소리", "세부 내용")
-        fun getInstance() : Notice {
-            return notice
-        }
-    }
 
 
 }
