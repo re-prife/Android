73d250579c7dbe60a1305dd304bad6ab23cc4f0c
KimSky904 <s2025@e-mirim.hs.kr>
2022-05-24 13:42:16 +0900

    Feat: 식재료 등록절차 XML작성, 식재료 data class
    식재료 정보입력 xml, 액티비티 이동

--- app/src/main/java/com/mirim/refrigerator/view/ingredient/IngredientRegisterActivity.kt
+++ app/src/main/java/com/mirim/refrigerator/view/ingredient/IngredientRegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.mirim.refrigerator.view.ingredient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mirim.refrigerator.R
 import com.mirim.refrigerator.databinding.ActivityHomeBinding
 import com.mirim.refrigerator.databinding.ActivityIngredientRegisterBinding
 
@@ -13,6 +15,20 @@ class IngredientRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIngredientRegisterBinding.inflate(layoutInflater)
         val view = binding.root
+
+
+        binding.btnMovePrev.setOnClickListener {
+            var intent = Intent(applicationContext,SelectIngredientRegisterType::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        binding.btnSaveIngredient.setOnClickListener {
+            // TODO : 식재료 저장
+        }
+        binding.btnCancelIngredient.setOnClickListener {
+            // TODO : 식재료 관리 탭 연결
+        }
+
 
         setContentView(view)
     }
--- app/src/main/java/com/mirim/refrigerator/view/ingredient/SelectIngredientRegisterType.kt
+++ app/src/main/java/com/mirim/refrigerator/view/ingredient/SelectIngredientRegisterType.kt
@@ -18,8 +18,22 @@ class SelectIngredientRegisterType : AppCompatActivity() {
 
         setContentView(view)
         binding.btnRegisterIngredient.setOnClickListener {
-            var intent = Intent(applicationContext,IngredientRegisterActivity::class.java)
+            val intent = Intent(applicationContext,IngredientRegisterActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
         }
+        binding.btnScanQr.setOnClickListener {
+            val intent = Intent(applicationContext,QrScanActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+
+
+        /* Todo : 식재료 관리 탭 연결
+        binding.btnMovePrev.setOnClickListener {
+            var intent = Intent(applicationContext,식재료관리탭::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        } */
     }
 }
