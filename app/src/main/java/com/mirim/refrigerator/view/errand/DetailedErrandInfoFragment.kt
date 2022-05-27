71d4aff7d71c307ab94e95c91d41d98eb6992a20
KimSky904 <s2025@e-mirim.hs.kr>
2022-05-27 10:54:09 +0900

    Feat : 심부름 세부, 목록 recyclerview UI #35

---
+++ app/src/main/java/com/mirim/refrigerator/view/errand/DetailedErrandInfoFragment.kt
@@ -0,0 +1,18 @@
+package com.mirim.refrigerator.view.errand
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mirim.refrigerator.R
+
+class DetailedErrandInfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detailed_errand_info, container, false)
+    }
+}
