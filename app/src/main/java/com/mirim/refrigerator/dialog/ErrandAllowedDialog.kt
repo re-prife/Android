70cfa648dcde08e8561ff260f3a15f4b8bc66a13
KimHaNeul <s2025@e-mirim.hs.kr>
2022-05-29 19:33:05 +0900

    Feat : 심부름 요청, 수락 Dialog (#43)

---
+++ app/src/main/java/com/mirim/refrigerator/dialog/ErrandAllowedDialog.kt
@@ -0,0 +1,12 @@
+package com.mirim.refrigerator.dialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mirim.refrigerator.R
+
+class ErrandAllowedDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_errand_allowed)
+    }
+}
