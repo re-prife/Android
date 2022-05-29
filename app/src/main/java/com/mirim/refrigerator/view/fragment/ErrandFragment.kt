45dc67dc3135538358a951f26afd4fb91117479c
KimHaNeul <s2025@e-mirim.hs.kr>
2022-05-29 18:43:39 +0900

    Feat : 심부름 등록, 목록, 세부 UI (#35,#41)

--- app/src/main/java/com/mirim/refrigerator/view/fragment/ErrandFragment.kt
+++ app/src/main/java/com/mirim/refrigerator/view/fragment/ErrandFragment.kt
@@ -1,11 +1,16 @@
 package com.mirim.refrigerator.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mirim.refrigerator.R
+import com.mirim.refrigerator.view.errand.CreateErrandActivity
+import com.mirim.refrigerator.view.login.SignupActivity
 
 class ErrandFragment: Fragment() {
     override fun onCreateView(
@@ -13,6 +18,16 @@ class ErrandFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_errand, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_errand, container, false)
+
+        val btnAddErrand : FloatingActionButton = view.findViewById(R.id.btn_add_errand)
+        btnAddErrand.setOnClickListener {
+            var intent = Intent(view.context, CreateErrandActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+
+
+        return view
     }
 }
