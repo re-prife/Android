e7149d4eb60a934f59ea78d1c1dfecfd67aa3aa8
KimSky904 <s2025@e-mirim.hs.kr>
2022-05-30 10:07:29 +0900

    Feat : 심부름 도착 및 수락 Dialog (#43)
    * ErrandFragment.kt에서 viewbinding을 사용하도록 수정
    
    * ErrandAllowedDialog.kt의 기본 dialog 구조 작성
    
    * ErrandArrivedDialog.kt의 기본 dialog 구조 작성
    
    * dialog_errand_allowed.xml 및 dialog_errand_arrived.xml의 사이즈 조절
    
    * fragment_errand.xml에서 팝업 확인 용도의 버튼 2개 생성

--- app/src/main/java/com/mirim/refrigerator/view/fragment/ErrandFragment.kt
+++ app/src/main/java/com/mirim/refrigerator/view/fragment/ErrandFragment.kt
@@ -1,31 +1,56 @@
 package com.mirim.refrigerator.view.fragment
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mirim.refrigerator.R
+import com.mirim.refrigerator.databinding.FragmentErrandBinding
+import com.mirim.refrigerator.dialog.ErrandAllowedDialog
+import com.mirim.refrigerator.dialog.ErrandArrivedDialog
 import com.mirim.refrigerator.view.errand.CreateErrandActivity
 import com.mirim.refrigerator.view.login.SignupActivity
 
 class ErrandFragment: Fragment() {
+
+    var _binding : FragmentErrandBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        val TAG = "태그"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view : View = inflater.inflate(R.layout.fragment_errand, container, false)
+        _binding = FragmentErrandBinding.inflate(inflater,container,false)
+        val view = binding.root
 
-        val btnAddErrand : FloatingActionButton = view.findViewById(R.id.btn_add_errand)
-        btnAddErrand.setOnClickListener {
+        binding.btnAddErrand.setOnClickListener {
             var intent = Intent(view.context, CreateErrandActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
         }
+
+        binding.btnArrivedDialog.setOnClickListener {
+            val dialog = ErrandArrivedDialog("심부름 제목", listOf("엄마","아빠"))
+            dialog.show(requireActivity().supportFragmentManager,"")
+        }
+        binding.btnAllowedDialog.setOnClickListener {
+            val dialog = ErrandAllowedDialog("심부름 제목", "아빠")
+            dialog.show(requireActivity().supportFragmentManager,"")
+        }
+
 
 
         return view
