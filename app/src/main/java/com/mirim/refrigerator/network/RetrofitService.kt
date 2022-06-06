3e0af6789339afbd82e7e33a4043405872ea14ad
KimSky904 <s2025@e-mirim.hs.kr>
2022-06-06 18:03:37 +0900

    Feat : 로그인 서버 연동 - 기본 구조 작성 (#49)
    * 테스트용 계정 정보를 작성하였습니다.
    
    * 로그인을 진행할 때, code 500과 함께 생성자 에러가 발생합니다. 이는 서버측과 추가로 이야기해 본 후 수정해야 할 것 같습니다.
    
    * SigninActivity.kt 에서 예외처리나 입력값 처리를 할 예정입니다.

--- app/src/main/java/com/mirim/refrigerator/network/RetrofitService.kt
+++ app/src/main/java/com/mirim/refrigerator/network/RetrofitService.kt
@@ -6,6 +6,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitService {
 
+    /*
+
+    name : 테스트
+    nickname : test
+    email : testing@naver.com
+    pw : 12345678
+
+     */
+
     companion object {
         var serviceAPI : ServiceAPI
         val BASE_URL : String = "http://52.204.65.160:8080/"
