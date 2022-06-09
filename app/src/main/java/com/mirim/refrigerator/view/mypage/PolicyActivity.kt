5af1bc9848e826724370c4b68366f644a9b854d0
KimSky904 <s2025@e-mirim.hs.kr>
2022-06-09 17:09:20 +0900

    Feat : Mypage policy연결 (#64)

--- app/src/main/java/com/mirim/refrigerator/view/mypage/PolicyActivity.kt
+++ app/src/main/java/com/mirim/refrigerator/view/mypage/PolicyActivity.kt
@@ -2,11 +2,21 @@ package com.mirim.refrigerator.view.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mirim.refrigerator.R
+import com.mirim.refrigerator.databinding.ActivityPolicyBinding
 
 class PolicyActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPolicyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_policy)
+        binding = ActivityPolicyBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        binding.toolbar.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
