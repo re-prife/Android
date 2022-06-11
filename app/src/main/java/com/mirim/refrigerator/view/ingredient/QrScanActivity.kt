bad76aacc384d8bdda77d0d4670b11296ce19116
KimHaNeul <s2025@e-mirim.hs.kr>
2022-06-11 19:53:54 +0900

    Feat : QR스캔 기능 구현 (#16)
    * QR코드 스캔 후 IngredientRegisterActivity.kt로 값을 자동 입력시키는 기능을 구현하였습니다.

--- app/src/main/java/com/mirim/refrigerator/view/ingredient/QrScanActivity.kt
+++ app/src/main/java/com/mirim/refrigerator/view/ingredient/QrScanActivity.kt
@@ -1,31 +1,16 @@
 package com.mirim.refrigerator.view.ingredient
 
 import android.Manifest
-import android.app.Activity
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.fragment.app.Fragment
-import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.DecodeCallback
-import com.budiyev.android.codescanner.ErrorCallback
-import com.budiyev.android.codescanner.ScanMode
-import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.CaptureManager
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.mirim.refrigerator.databinding.ActivityQrScanBinding
-import com.mirim.refrigerator.dialog.ConfirmBarcodeScanDialog
-import com.mirim.refrigerator.dialog.QRErrorDialog
-import com.mirim.refrigerator.model.Barcode
-import com.mirim.refrigerator.extension.usecase.Settings
 
-class QrScanActivity : AppCompatActivity(), ConfirmBarcodeScanDialog.Listener {
+class QrScanActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQrScanBinding
-
-    private lateinit var codeScanner : CodeScanner
-    private var qrScanResult : Barcode? = null
+    private lateinit var capture : CaptureManager
 
     companion object {
         private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
@@ -38,51 +23,41 @@ class QrScanActivity : AppCompatActivity(), ConfirmBarcodeScanDialog.Listener {
         val view = binding.root
         setContentView(view)
 
+        val barcodeScanner : DecoratedBarcodeView = binding.zxingBarcodeScanner
+        capture = CaptureManager(this,barcodeScanner)
+        capture.initializeFromIntent(intent, savedInstanceState)
+        capture.decode()
+
+
+
         binding.toolbar.btnBack.setOnClickListener {
             finish()
         }
 
     }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
-
+    override fun onResume() {
+        super.onResume()
+        capture.onResume()
     }
 
-    /* 카메라 초기화 설정 (sharedPreferences 사용) */
-    private fun initScanner() {
-//        codeScanner = CodeScanner(this,binding.scannerQRCode).apply {
-//            camera = if(Settings.getInstance(requireContext()).isBackCamera) {
-//                CodeScanner.CAMERA_BACK
-//            } else {
-//                CodeScanner.CAMERA_FRONT
-//            }
-//            formats = listOf(BarcodeFormat.QR_CODE)
-//            scanMode = ScanMode.SINGLE
-//            isAutoFocusEnabled = true
-//            isTouchFocusEnabled = false
-//
-//            TODO : handleScannedBarcode 구현, showError dialog 구현
-//            decodeCallback = DecodeCallback(::handleScannedBarcode)
-//            errorCallback = ErrorCallback(::showError)
-//        }
+    override fun onPause() {
+        super.onPause()
+        capture.onPause()
     }
 
-    override fun onBarcodeConfirmed(barcode: Barcode) {
-        handleConfirmedBarcode(barcode)
+    override fun onDestroy() {
+        super.onDestroy()
+        capture.onDestroy()
     }
 
-    override fun onBarcodeDeclined() {
-        restartPreview()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        capture.onSaveInstanceState(outState)
     }
 
-    private fun handleConfirmedBarcode(barcode: Barcode) {
-
-    }
-
-    private fun restartPreview() {
-        this@QrScanActivity.runOnUiThread {
-            codeScanner.startPreview()
-        }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
