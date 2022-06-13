f33e23aa1779572deeeae1e62799889d6bbe3b94
KimHaNeul <s2025@e-mirim.hs.kr>
2022-06-13 23:15:06 +0900

    Feat : 회원 비밀번호 수정 기능 구현 (#71)
    * UserAPI.kt에서 프로필 수정, 프로필 이미지 수정, 비밀번호 변경 fun을 작성하였습니다.

--- app/src/main/java/com/mirim/refrigerator/network/UserAPI.kt
+++ app/src/main/java/com/mirim/refrigerator/network/UserAPI.kt
@@ -1,18 +1,40 @@
 package com.mirim.refrigerator.network
 
+import com.mirim.refrigerator.server.request.ModifyUserInfoRequest
+import com.mirim.refrigerator.server.request.ModifyUserPasswordRequest
 import com.mirim.refrigerator.server.responses.HomeKingsResponse
+import com.mirim.refrigerator.server.responses.Response
 import com.mirim.refrigerator.server.responses.UserInfoResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface UserAPI {
     // 회원 상세 정보 조회
     @GET("users/{userId}")
     fun getUserData(
-        @Path("userId") groupId : Int,
+        @Path("userId") uesrId : Int,
         @Query("date") date : String
     ): Call<UserInfoResponse>
+
+    // 프로필 수정
+    @PUT("users/{userId}")
+    fun modifyUser(
+        @Path("userId") userId : Int,
+    ): Call<Response>
+
+    // 프로핗 이미지 수정
+    @POST("uploads/users/{userId}")
+    fun modifyUserImage(
+        @Path("userId") userId : Int,
+        @Body profileImage : MultipartBody.Part
+    ): Call<Response>
+
+    // 사용자 비밀번호 변경
+    @PUT("users/{userId}/password")
+    fun modifyUserPassword(
+        @Path("userId") userId: Int,
+        @Body body : ModifyUserPasswordRequest,
+    ): Call<Response>
 
 }
