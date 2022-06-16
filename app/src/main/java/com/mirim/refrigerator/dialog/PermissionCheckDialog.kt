69e0ad38539c2a606ce813cbf14521e509bc17f2
KimHaNeul <s2025@e-mirim.hs.kr>
2022-06-16 09:14:55 +0900

    Feat : 권한 안내 Dialog Fragment 구현 (#83)

--- app/src/main/java/com/mirim/refrigerator/dialog/PermissionCheckDialog.kt
+++ app/src/main/java/com/mirim/refrigerator/dialog/PermissionCheckDialog.kt
@@ -1,27 +1,28 @@
 package com.mirim.refrigerator.dialog
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
 import com.mirim.refrigerator.R
 import com.mirim.refrigerator.databinding.ActivityPermissionCheckDialogBinding
 
-class PermissionCheckDialog(context : Context) {
+class PermissionCheckDialog() : DialogFragment() {
 
     lateinit var binding : ActivityPermissionCheckDialogBinding
-    private val dialog = Dialog(context)
 
-    fun showDialog()
-    {
-        dialog.setContentView(R.layout.custom_dialog)
-        dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
-        dialog.setCanceledOnTouchOutside(true)
-        dialog.setCancelable(true)
-        dialog.show()
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(context)
+            binding = ActivityPermissionCheckDialogBinding.inflate(requireActivity().layoutInflater)
 
-        binding.btnAccept.setOnClickListener {
-            dialog.dismiss()
-        }
+            binding.btnAccept.setOnClickListener {
+                dialog?.dismiss()
+            }
+            builder.setView(binding.root)
+            builder.create()
+        }?: throw IllegalStateException("Activity cannot be null")
     }
 }
